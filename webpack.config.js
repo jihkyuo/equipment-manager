ae8c4465bd4ae0c038469a1cbe214508f21fa426
jihkyuo <84238602+jihkyuo@users.noreply.github.com>
2021-11-24 22:34:57 +0900

    Webpack config
    SCSS파일 분리관리(MiniCssExtractPlugin)
    
    이외 package.json 설정변경

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
+// SCSS를 분리해서 관리하기 위해 MiniCssExtractPlugin 가져옴
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 const path = require("path");
 
 module.exports = {
   entry: "./src/client/js/main.js",
   mode: "development",
+  watch: true,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "assets", "js"),
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"),
+    clean: true,
   },
   module: {
     rules: [
@@ -20,7 +30,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
